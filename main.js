776627955576bbabeefa18cd39d951122950329c
Eoin Ocathasaigh <g00417466@atu.ie>
2025-04-27 16:29:16 +0100

    Added some comments to the main.js page

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { app: electronApp, BrowserWindow } = require('electron');
 const expressApp = require('./index');      // your Express app
 let expressServer, mainWindow;
 
+//Various methods for handling opening and closing the express server and the electron app
 async function startExpress() {
   return new Promise(resolve => {
     expressServer = expressApp.listen(4000, () => {
@@ -12,10 +13,13 @@ async function startExpress() {
   });
 }
 
+//Shutting down the express server cleanly
+//This is important to avoid port conflicts when restarting the app
 function stopExpress() {
   if (expressServer) expressServer.close();
 }
 
+//Function to create the main window of the Electron app
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200, height: 800,
